5399e951afbadd2d9e2b13c9c48de0d97ac2db6c
Félix <f.courtemanche@rubika-edu.com>
2020-12-02 16:58:01 +0100

    Jépouche mais c'est pas la joie

---
+++ Assets/Scripts/FCO_Scripts/FCO_SpecificObjectScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FCO_SpecificObjectScript : MonoBehaviour
+{
+    public enum ItemType {Ingredient, Recipient, None};
+    public ItemType type;
+
+    public bool burnable = false;
+
+    public void UseBehaviour()
+    {
+        switch (type)
+        {
+            case ItemType.Ingredient:
+                {
+
+                    break;
+                }
+            case ItemType.Recipient:
+                {
+
+                    break;
+                }
+        }
+    }
+
+}
