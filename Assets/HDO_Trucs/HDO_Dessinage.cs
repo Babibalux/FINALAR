eed635db0d3d8024463482c483bbd9a97052d2fb
PALADIOUM <h.dontzow@rubika-edu.com>
2020-12-03 14:48:38 +0100

    dessinage.dessinage

---
+++ Assets/HDO_Trucs/HDO_Dessinage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HDO_Dessinage : MonoBehaviour
+{
+    public TrailRenderer trail;
+    public bool dessinage = true;
+
+    void Start()
+    {
+        trail = GetComponent<TrailRenderer>();
+    }
+
+    void Update()
+    {
+        if(Input.touchCount > 0 && dessinage)
+        {
+            trail.emitting = true;
+        }
+        else
+        {
+            trail.emitting = false;
+        }
+        trail.time += Time.deltaTime;
+    }
+}
---
+++ Assets/HDO_Trucs/HDO_RoutePentacle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HDO_RoutePentacle : MonoBehaviour
+{
+    ParticleSystem particles;
+    public HDO_Dessinage dessinage;
+    Collider box;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        box = GetComponent<Collider>();
+        particles = GetComponent<ParticleSystem>();
+        particles.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Cursor")
+        {
+            if (dessinage.dessinage && dessinage.trail.emitting)
+            {
+                particles.Play();
+            }
+        }
+    }
+}
