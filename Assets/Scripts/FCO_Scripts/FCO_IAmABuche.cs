c587e3638117bb2bf741ba88c76183a532fc74e6
Félix <f.courtemanche@rubika-edu.com>
2020-12-03 15:32:19 +0100

    BRULER

---
+++ Assets/Scripts/FCO_Scripts/FCO_IAmABuche.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FCO_IAmABuche : MonoBehaviour
+{
+    public bool burned = false;
+    public Mesh cendre3D;
+}
