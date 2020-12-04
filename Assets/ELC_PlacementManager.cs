e6c28f95c7b76656cf4d4a632df0fabf635b4faa
Eliott <eliott.chauchard@orange.fr>
2020-12-04 17:28:54 +0100

    push viteeeeeee

--- Assets/ELC_PlacementManager.cs
+++ Assets/ELC_PlacementManager.cs
@@ -11,19 +11,37 @@ public class ELC_PlacementManager : MonoBehaviour
     public List<GameObject> candlesPlacements = new List<GameObject>(); //Nord, Est, Sud-Est, Sud-Ouest, Ouest
 
     public List<string> objectsOrder = new List<string>();
-    public List<string> candlesOrder = new List<string>();
+    public List<string> candlesTagOrder = new List<string>();
 
-    private void Start()
-    {
-        
-    }
 
     private void Update()
     {
+        verifiedCandles = 0;
+        verifiedObjects = 0;
+
         for (int i = 0; i < objectsPlacements.Count; i++)
         {
+            if (objectsPlacements[i].GetComponent<ELC_Emplacement>().objectPlaced != null && objectsOrder[i] != null)
+            {
+                if (objectsPlacements[i].GetComponent<ELC_Emplacement>().objectPlaced.GetComponent<FCO_ItemsScriptableObjects>().ingredientType.ToString() == objectsOrder[i]) verifiedObjects++;
+            }
 
+            if (candlesPlacements[i].GetComponent<ELC_Emplacement>().objectPlaced != null)
+            {
+                if (candlesTagOrder[i] == candlesPlacements[i].GetComponent<ELC_Emplacement>().objectPlaced.GetComponent<FCO_ItemsScriptableObjects>().ingredientType.ToString()) verifiedCandles++;
+            }
         }
+
+        if(verifiedCandles >= 5)
+        {
+            //Bougies vérifiées
+        }
+
+        if(verifiedObjects >= 3)
+        {
+            //Objets vérifiés
+        }
+
     }
 
 }
