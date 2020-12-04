7aabbacf54803782a644220d6b9746c2e3c0ae48
Félix <f.courtemanche@rubika-edu.com>
2020-12-04 21:57:41 +0100

    Débux

--- Assets/Scripts/FCO_Scripts/FCO_ContentementManagement.cs
+++ Assets/Scripts/FCO_Scripts/FCO_ContentementManagement.cs
@@ -7,6 +7,9 @@ public class FCO_ContentementManagement : MonoBehaviour
     public int contentement;
     HDO_Dialogues dialogues;
 
+    public GameObject demonBG;
+    public GameObject demimonde;
+
     private void Start()
     {
         dialogues = GameObject.FindObjectOfType<HDO_Dialogues>();
@@ -14,19 +17,56 @@ public class FCO_ContentementManagement : MonoBehaviour
 
     public void Invocation()
     {
-        if(contentement >= 0)
+        if (FindObjectOfType<ELC_PlacementManager>() != null && FindObjectOfType<HDO_RoutePentacle>() != null)
         {
-            //pliz déplacer ça A LINTERIEUR du moment ou on INVOQUE EL DEMONO DE LA CASA
-            dialogues.sequence = 5;
-            dialogues.shouldWrite = true;
-            //invoquer beau démon
+            ELC_PlacementManager placementManager = FindObjectOfType<ELC_PlacementManager>();
+
+            if (placementManager.objectsAreGood)
+            {
+                contentement++;
+            }
+            else contentement--;
+
+            if (placementManager.candlesAreGood)
+            {
+                contentement++;
+            }
+            else contentement--;
+
+            HDO_RoutePentacle routePentacle = FindObjectOfType<HDO_RoutePentacle>();
+
+            if (routePentacle.GoodDessin)
+            {
+                contentement++;
+            }
+            else if (routePentacle.pentacleFail)
+            {
+                contentement--;
+            }            
+        }
+        else contentement--;
+
+        if(GameObject.FindGameObjectWithTag("Puits"))
+        {
+            if (contentement >= 0)
+            {
+                Instantiate<GameObject>(demonBG, GameObject.FindGameObjectWithTag("Puits").transform);
+                dialogues.sequence = 5;
+                dialogues.shouldWrite = true;
+            }
+            else
+            {
+                Instantiate<GameObject>(demimonde, GameObject.FindGameObjectWithTag("Puits").transform);
+                dialogues.sequence = 6;
+                dialogues.shouldWrite = true;
+            }
         }
         else
         {
-            //pliz déplacer ça A LINTERIEUR du moment ou on INVOQUE EL DEMONO DE LA CASA
-            dialogues.sequence = 6;
-            dialogues.shouldWrite = true;
-            //invoquer moche démon
+                Instantiate<GameObject>(demimonde, null);
+                dialogues.sequence = 6;
+                dialogues.shouldWrite = true;
         }
+        
     }
 }
