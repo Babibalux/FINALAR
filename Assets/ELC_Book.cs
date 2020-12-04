eaf4f229a3c5b7ccf6a507ce0df797149d333e81
Eliott <eliott.chauchard@gmail.com>
2020-12-04 23:56:14 +0100

    lgn

---
+++ Assets/ELC_Book.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ELC_Book : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Start()
+    {
+        this.transform.rotation = Quaternion.Inverse(GameObject.FindWithTag("Cursor").transform.rotation);
+    }
+    void Update()
+    {
+        cam = FindObjectOfType<Camera>();
+        this.transform.localPosition = new Vector3(transform.localPosition.x, GameObject.FindWithTag("Puits").transform.localPosition.y, transform.localPosition.z);
+        
+    }
+}
