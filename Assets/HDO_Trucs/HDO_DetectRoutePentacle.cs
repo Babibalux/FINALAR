e7b55a2c315dc9c25cff49c4072f3042e33c8c5f
PALADIOUM <h.dontzow@rubika-edu.com>
2020-12-04 12:27:03 +0100

    EL PENTACLO DE LA INVOCATION DEL SATANO

--- Assets/HDO_Trucs/HDO_DetectRoutePentacle.cs
+++ Assets/HDO_Trucs/HDO_DetectRoutePentacle.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class HDO_DetectRoutePentacle : MonoBehaviour
 {
-    public bool detected = false;
+    public bool detected = false, hasDetected = false;
+    HDO_Dessinage dessinage;
+    ParticleSystem particles;
     Collider box;
     Rigidbody rb;
+    float waitStart = 2;
 
     // Start is called before the first frame update
     void Start()
     {
+        dessinage = GameObject.Find("dessinage").GetComponent<HDO_Dessinage>();
+        particles = GetComponent<ParticleSystem>();
+        particles.Stop();
         box = GetComponent<Collider>();
         rb = GetComponent<Rigidbody>();
     }
@@ -18,14 +24,16 @@ public class HDO_DetectRoutePentacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        waitStart -= Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Cursor"))
+        if (other.CompareTag("Cursor") && !hasDetected && dessinage.trail.emitting && waitStart <=0)
         {
             detected = true;
+            particles.Play();
+            hasDetected = true;
             box.enabled = false;
         }
     }
--- Assets/HDO_Trucs/HDO_RoutePentacle.cs
+++ Assets/HDO_Trucs/HDO_RoutePentacle.cs
@@ -8,9 +8,13 @@ public class HDO_RoutePentacle : MonoBehaviour
     public HDO_Dessinage dessinage;
     public List<HDO_DetectRoutePentacle> route = null;
     public List<HDO_DetectRoutePentacle> chemin = null;
-    int nextPoint;
+    int nextPointR, nextPointL;
     int iteration;
-    bool pentacleFail = false;
+    bool pentacleFail = false, checkPentacle = true, pentacleDone = false;
+
+    int routeType = -1;
+    int checkedRoute = 0;
+    public float timeForPentacle;
 
     // Start is called before the first frame update
     void Start()
@@ -23,39 +27,133 @@ public class HDO_RoutePentacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DecideRouteOK();
+        if (pentacleFail)
+            Destroy(this.gameObject);
+
+        if (checkPentacle) 
+            DecideRouteOK(); 
+        
+        if(pentacleDone) 
+            PentacleEnded();
+
+        timeForPentacle -= Time.deltaTime;
+
+        if(timeForPentacle <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        foreach(HDO_DetectRoutePentacle point in route)
+        {
+            if (point.hasDetected) checkedRoute++;
+            else checkedRoute = 0;
+        }
+
+        if (checkedRoute == 5) pentacleDone = true;
+        
+    }
+
+    void PentacleEnded()
+    {
+        if (pentacleFail) Destroy(gameObject);
+        particles.Play();
+        dessinage.dessinage = false;
     }
 
     void DecideRouteOK()
     {
-        foreach(HDO_DetectRoutePentacle point in route)
+        if(routeType == -1)
         {
-            if(iteration > 4)
+            if (route[0].hasDetected)
             {
-                iteration = 0;
+                chemin.Add(route[0]);
+                nextPointL = 4;
+                nextPointR = 1;
+                routeType = 0;
             }
-            if (point.detected)
+            if (route[1].hasDetected)
             {
-                chemin.Add(point);
-                if(route[nextPoint] != route[iteration])
-                {
-                    pentacleFail = true;
-                    iteration = -1;
-                }
-                else
-                {
-                    nextPoint = iteration + 1;
-                }
-                point.detected = false;
-
+                chemin.Add(route[1]);
+                nextPointL = 2;
+                nextPointR = 0;
+                routeType = 1;
             }
-            iteration++;
+            if (route[2].hasDetected)
+            {
+                chemin.Add(route[2]);
+                nextPointL = 3;
+                nextPointR = 2;
+                routeType = 2;
+            }
+            if (route[3].hasDetected)
+            {
+                chemin.Add(route[3]);
+                nextPointL = 4;
+                nextPointR = 2;
+                routeType = 3;
+            }
+            if (route[4].hasDetected)
+            {
+                chemin.Add(route[4]);
+                nextPointL = 3;
+                nextPointR = 0;
+                routeType = 4;
+            }
         }
-
-        if(chemin.Count >= 5)
+        else
         {
-            dessinage.dessinage = false;
-            particles.Play();
+            if(chemin.Count >= 5)
+            {
+                pentacleDone = true;
+            }
+
+            foreach (HDO_DetectRoutePentacle point in route)
+            {
+                if (point.hasDetected)
+                {
+                    if (!(chemin.Contains(point))) chemin.Add(point);
+                    else
+                    {
+                        return;
+                    }
+                    point.detected = false;
+                    if (point == route[nextPointL] || point == route[nextPointR])
+                    {
+                       
+                        if (point == route[1])
+                        {
+                            nextPointL = 2;
+                            nextPointR = 0;
+                        }
+                        if (point == route[2])
+                        {
+                            nextPointL = 3;
+                            nextPointR = 2;
+                        }
+                        if (point == route[3])
+                        {
+                            nextPointL = 2;
+                            nextPointR = 4;
+                        }
+                        if (point == route[4])
+                        {
+                            nextPointL = 3;
+                            nextPointR = 0;
+                        }
+                        if (point == route[0])
+                        {
+                            nextPointL = 4;
+                            nextPointR = 1;
+                        }
+                    }
+                    else
+                    {
+                        pentacleFail = true;
+                        checkPentacle = false;
+                    }
+                }
+            }
         }
+        
     }
 }
