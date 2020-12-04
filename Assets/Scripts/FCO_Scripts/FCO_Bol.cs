07768056d44eeb49a8b3d8efa6750f598892c3b4
Félix <f.courtemanche@rubika-edu.com>
2020-12-04 11:07:12 +0100

    Fock

---
+++ Assets/Scripts/FCO_Scripts/FCO_Bol.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FCO_Bol : MonoBehaviour
+{
+    public List<FCO_ItemsScriptableObjects.IngredientType> typeContenu = new List<FCO_ItemsScriptableObjects.IngredientType>();
+    public List<bool> etatContenu = new List<bool>();
+
+    public void PutInTheBowl(FCO_ItemsScriptableObjects.IngredientType type, bool etat)
+    {
+        typeContenu.Add(type);
+        etatContenu.Add(etat);
+    }
+
+    public void TransferContent(List<FCO_ItemsScriptableObjects.IngredientType> destinationType, List<bool> destinationState)
+    {
+        for (int i = 0; i < typeContenu.Count; i++)
+        {
+            destinationType.Add(typeContenu[i]);
+            destinationState.Add(etatContenu[i]);
+        }
+
+        typeContenu = new List<FCO_ItemsScriptableObjects.IngredientType>();
+        etatContenu = new List<bool>();
+    }
+}
